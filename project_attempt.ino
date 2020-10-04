5c136d1bd03040fb04d68cf3cc8a1e8d8260f6a5
滑板鞋 <aijm73@gmail.com>
2020-10-04 19:24:13 +0800

    Update project_attempt.ino

--- project_attempt.ino
+++ project_attempt.ino
@@ -7,42 +7,55 @@
  *  ?/?   -   File created
  *  ?.?   -   Basic structure created
  *  3/10  -   Merged changes from basic_test
+ *        -   Refined comments
+ *        -   Removed unecessary constants (See old vers. if required)
+ *        -   Updated from using pin-read interrupt to using capture
  */
 #include <avr/io.h>
 #include <avr/interrupt.h>
 
 #define N_DIGITS 4
-#define N_SELECT 3
 #define DECIMAL_PLACES 2
 #define DEFAULT_WHEEL_CIRCUMFERENCE 2.105
+// Mapping for array values[] (See below)
+#define N_VALUES 3
+#define DISTANCE 0
+#define SPEED_MAX 1
+#define SPEED_AVE 2
 
 
+#define TIMER1_RESET TCNT1 = 0
+
+// Summary of Pins:
 // Inputs of Rotary Encoder Switch
-const int ENCB = 0;
-const int ENCA = 1;
-const int RESET = 2;
-const int SETTINGS = 3;
+// ENCB       0   D0
+// ENCA       1   D1
+// RESET      2   D2
+// SETTINGS   3   D3
 
 // Sensor in
-const int SENS = 8; // B0
+// SENS       8   B0
 
 // Outputs to BCD decoder
-const int OUT_A = 14; // C0, lowest significant bit
-const int OUT_B = 15;
-const int OUT_C = 16;
-const int OUT_D = 17; // C3
-const int DECIMAL = 18; // C4
+// OUT_A      14  C0      lowest significant bit
+// OUT_B      15  C1
+// OUT_C      16  C2
+// OUT_D      17  C3
+// DECIMAL    18  C4
 
 // Outputs to LED's (For mode)
-const int MODE_A = 4; // D4
-const int MODE_B = 5; // D5
-const int MODE_C = 9; // B1
-const int MODE_D = 10; // B2
+// MODE_A     4   D4
+// MODE_B     5   D5
+// MODE_C     9   B1
+// MODE_D     10  B2
 
 // Outputs to Demultiplexer
-const int DEM_0 = 11; // B3
-const int DEM_1 = 12; // B4
-const int DEM_2 = 13; // B5
+// DEM_0      11  B3
+// DEM_1      12  B4
+// DEM_2      13  B5
+
+#define OUT_A 14
+#define DEM_0 11
 
 // Used in case the position of pins is modified
 const int DEM_OFFSET = DEM_0 - 8;
@@ -54,30 +67,42 @@ int digitMap[8] = {0, 1, 2, 3, 4, 5, 6, 7};
 volatile int digitSelect;
 
 volatile double circumference;
-double velocity = 0;
-int seconds = 0;
-double distance = 0;
-volatile char num[N_DIGITS];
 
+// One display used for strictly speed
+float speed;
+
+// Structure used to hold values
+// value = {DISTANCE, SPEED_MAX, SPEED_AVE};
+float value[N_VALUES] = {0.0, 0.0, 0.0};
+volatile int revolutions;
+volatile int setting;
+
+// Number to be shown on first display
+// Defaulted to strictly show speed
+volatile char num[N_DIGITS];
+// Number to be shown on second display
+// Shows data from value[] depending on setting
+volatile char num2[N_DIGITS];
 
 
 void setup() {
-  pinMode(SENS, INPUT);
+  // Configure ports 
+  // Sensor input
+  // DDRB = DDRB & ~(_BV(0));
+  // Rotary Encoder + Buttons {ENCA, ENCB, RESET, SETTINGS}
+  DDRD = DDRD & ~(_BV(0) | _BV(1) | _BV(2) | _BV(3));
 
-  pinMode(ENCA, INPUT);
-  pinMode(ENCB, INPUT);
-  pinMode(RESET, INPUT);
-  pinMode(SETTINGS, INPUT);
- 
+  // Demultiplexer + Settings LEDs
   DDRB = DDRB | _BV(1) | _BV(2) | _BV(3) | _BV(4) | _BV(5);
   PORTB = PORTB & ~(_BV(1) | _BV(2) | _BV(3) | _BV(4) | _BV(5));
+  // Settings LEDs
   DDRD = DDRD | _BV(4) | _BV(5);
   PORTD = PORTD & ~(_BV(4) | _BV(5));
-
+  // BCD Decoder + Decimal point
   DDRC = DDRC | _BV(0) | _BV(1) | _BV(2) | _BV(3) | _BV(4);
   PORTC = PORTC & ~(_BV(0) | _BV(1) | _BV(2) | _BV(3) | _BV(4));
 
-  cli();
+  cli(); // Disable interrupts
 
   // TIMER2 set up to display digit
   // NOTICE:  TIMER0 is not used because TIMER0 overflow is by default in use
@@ -88,24 +113,44 @@ void setup() {
   TCCR2B = _BV(CS21); // 8 bit prescaler - Increase prescaler to increase period
   TIMSK2 = _BV(TOIE2); // Enables the use of timer overflow interrupt
 
-  // Timer 1 set up
+  // TIMER1 set up to time the wheel
+  // A maximum period is set in order to detect idleness
   TCCR1A = 0;
-  // CS10 - CS12  :  Manages prescaler - Set to
-  TCCR1B = _BV(CS10);
-  TCNT1 = 0; // Clears Timer Count
+  TCCR1B = _BV(CS10) | _BV(CS12); // 16 bit prescaler - Set to 1024 
+
+  // Enables CTC mode comparison with OCR1A  
+  // Note that the physical pin itself is not connected for the timer
+  TCCR1B = TCCR1B | _BV(WGM12);
+
+  TIMSK1 = _BV(OCIE1A); // Enables use of timer compare interrupt
+
+  // Set the maximum count for the timer
+  // The formula is given as:   OCR1A = (Clock Frequency) / (N * (Desired Frequency)) - 1
+  //                            where N is the set prescaler {1, 8, 64, 256, 1024}
+  OCR1A = 46874; // Set to 3 seconds
+  
+  // Enables Capture mode to read input
+  // ICNC1 - Noise Canceler, ICES1 - Rising edge triggered input capture
+  TCCR1B = TCCR1B | _BV(ICNC1) | _BV(ICES1);
+
+  // Enables capture interrupt
+  TIMSK1 = TIMSK1 | _BV(ICIE1);
 
 
+  sei(); // Enable interrupts
 
-  sei();
-
-  formatOutput(0.0, num);
-  circum = DEFAULT_WHEEL_CIRCUMFERENCE;
-  seconds = 0;
+  speed = 0.0;
+  formatOutput(0.0, num); // Sets num to 0
+  circumference = DEFAULT_WHEEL_CIRCUMFERENCE;
   digitSelect = 0;
+  
 
-  attachInterrupt(SENS, updateSens, RISING);
+  setting = DISTANCE; // default
+
+  // attachInterrupt(SENS, updateSens, RISING);
   attachInterrupt(RESET, reset, RISING);
   attachInterrupt(SETTINGS, changeSetting, RISING);
+  TIMER1_RESET;
 }
 
 void loop() {
@@ -115,42 +160,55 @@ void loop() {
 
 /*
  * When TIMER2 overflows, a digit is displayed
+ * Cycles through digits, displaying a value specified in num
+ * to one display, and the value in num2 to the other
+ * This MAY not work correctly  : consider increasing prescaler?
  */
 ISR(TIMER2_OVF_vect) {
-  displayValue(0); 
-  // change to 1 to test other display
-  // displayValue(num, 1);
-}
-
-void updateSens() {
-  //TBD
+  int decimal = 0;
+  if(digitSelect == 1 || digitSelect == 5) decimal++;
+  setDemulti(digitMap[digitSelect]);
+  if(digitSelect < 4) {
+    setDisplay(num[digitSelect]);
+  } else {
+    setDisplay(num2[digitSelect - N_DIGITS]);
+  }
+  digitSelect = (digitSelect + 1) % (N_DIGITS * 2);
 }
 
 /*
- *  Resets 
+ * When TIMER1 reaches OCR1A, the bike is assumed to be idle
+ */
+ISR(TIMER1_COMPA_vect) {
+  //
+  speed = 0.0;
+  formatOutput(0.0, num);
+
+}
+
+/*
+ * Input from hall sensor 
+ */
+ISR(TIMER1_CAPT_vect) {
+  //TBD
+
+
+  TIMER1_RESET;
+}
+
+/*
+ *  Resets distance
  */ 
 void reset() {
-  //TBD
+  value[0] = 0;
 }
 
-/*
- * Cycles through digits, displaying a value specified in num
- * displayN (0 - 1) specifies the display
- */
-void displayValue(int displayN) {
-  int decimal = 0;
-  if(digitSelect == 1) decimal++;
-  setDemulti(digitMap[digitSelect + (displayN * N_DIGITS)]);
-  setDisplay(num[digitSelect], decimal);
-  digitSelect = (digitSelect + 1) % N_DIGITS;
-
-}
 
 /*
- *  Updates setting
+ *  Toggles value to display in num2
  */
 void changeSetting() {
-  //TBD
+  setting = (setting + 1) % N_VALUES;
 }
 
 /*
